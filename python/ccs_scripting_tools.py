b732bdb3e9fb96d1a9571b58023050d3fb6e3ee0
Jim Chiang <jchiang@slac.stanford.edu>
2017-04-25 13:29:38 -0700

    add unit tests and docstrings

--- python/ccs_scripting_tools.py
+++ python/ccs_scripting_tools.py
@@ -4,45 +4,86 @@ Tools for CCS jython scripts.
 try:
     from org.lsst.ccs.scripting import CCS
 except ImportError:
+    # Create non-jython objects and classes to enable testing.
     class CcsType(object):
+        "Non-jython proxy for org.lsst.ccs.scripting.CCS"
+
         def attachSubsystem(self, value):
+            """
+            Attach a do-nothing object that acts like a CCS subsystem.
+            """
             return NullSubsystem()
 
     CCS = CcsType()
 
     class NullSubsystem(object):
+        """
+        A do-nothing class with dummy methods provided by a CCS subsystem.
+        """
         def __init__(self):
             pass
+
         def synchCommand(self, *args):
+            "Execute a synchronous CCS command."
             return NullResponse()
+
         def asynchCommand(self, *args):
+            "Execute an asynchronous CCS command."
             return NullResponse()
 
     class NullResponse(object):
+        """
+        Do-nothing response class to act as a return object by the
+        NullSubsystem methods.
+        """
         def __init__(self):
             pass
+
         def getResult(self):
+            "A generic result."
             return 1
 
 class SubsystemDecorator(object):
+    """
+    Decorator class to overlay logging of the commands sent to a CCS
+    subsystem object.
+    """
     def __init__(self, ccs_subsystem, logger=None):
         self.ccs_subsystem = ccs_subsystem
         self.logger = logger
 
-    def synchCommand(self, *args):
+    def _log_command(self, args):
         if self.logger is not None:
-            self.logger.info(args[1])
+            command_string = " ".join(["%s" % arg for arg in args])
+            self.logger.info(command_string)
+
+    def synchCommand(self, *args):
+        "Decorator method for a synchronous command."
+        self._log_command(args)
         return self.ccs_subsystem.synchCommand(*args)
 
     def asynchCommand(self, *args):
-        if self.logger is not None:
-            self.logger.info(args[0])
+        "Decorator method for an asynchronous command."
+        self._log_command(args)
         return self.ccs_subsystem.asynchCommand(*args)
 
 class CcsSubsystems(object):
-    def __init__(self, subsystems=None, logger=None):
-        if subsystems is None:
-            subsystems = dict(ts8='ts8')
+    """
+    Container for collections of CCS subsystems.
+    """
+    def __init__(self, subsystems, logger=None):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        subsystems : dict
+            A dictionary of subsystem names keyed by desired attribute
+            name, e.g., dict(ts8='ts8', pd='ts/PhotoDiode',
+                             mono='ts/Monochromator')
+        logger : logging.Logger, optional
+            Logger to be used by the SubsystemDecorator class. Default: None.
+        """
         for key, value in subsystems.items():
             self.__dict__[key] = SubsystemDecorator(CCS.attachSubsystem(value),
                                                     logger=logger)
