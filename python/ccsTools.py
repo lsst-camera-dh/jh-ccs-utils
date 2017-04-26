368841f5cb55f9a1afc59bc0eac92653fcabf1c3
Jim Chiang <jchiang@slac.stanford.edu>
2017-04-26 13:31:39 -0700

    no need to make default dict entries for ccd_names

--- python/ccsTools.py
+++ python/ccsTools.py
@@ -84,7 +84,7 @@ class CcsRaftSetup(CcsSetup):
         super(CcsRaftSetup, self).__init__(configFile)
         self.commands.append('from collections import namedtuple')
         self.commands.append("SensorInfo = namedtuple('SensorInfo', 'sensor_id manufacturer_sn'.split())")
-        self.commands.append("ccd_names = dict([(slot, ()) for slot in 'S00 S01 S02 S10 S11 S12 S20 S21 S22'.split()])")
+        self.commands.append("ccd_names = dict()")
         self._get_ccd_names()
     def _get_ccd_names(self):
         raft_id = siteUtils.getUnitId()
@@ -92,7 +92,8 @@ class CcsRaftSetup(CcsSetup):
         for slot in raft.slot_names:
             sensor = raft.sensor(slot)
             self.set_item('ccd_names["%s"]' % slot, 'SensorInfo("%s", "%s")'
-                          % (str(sensor.sensor_id), str(sensor.manufacturer_sn)))
+                          % (str(sensor.sensor_id),
+                             str(sensor.manufacturer_sn)))
         ccd_type = str(raft.sensor_type.split('-')[0])
         self['ccd_type'] = ccd_type
         if ccd_type == 'ITL':
