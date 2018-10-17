173668409a776b9b4b04537c71ec4668e4f26bc0
James Chiang <jchiang@slac.stanford.edu>
2018-10-16 19:51:42 -0700

    refactor to make more pythonic

--- python/PythonBinding.py
+++ python/PythonBinding.py
@@ -1,140 +1,138 @@
-#!/usr/bin/python
+"""
+Socket connection interface to CCS Jython interpreter.
+"""
+import sys
+import time
+import re
 import socket
 import threading
-import time
-import random
-import re
-import sys
+import uuid
+
+__all__ = ['CcsJythonInterpreter', 'CcsException', 'CcsExecutionResult']
+
 
 class CcsExecutionResult:
+    """Results class."""
     def __init__(self, thread):
         self.thread = thread
 
-    def isRunning(self):
-        return self.thread.running
-
     def getOutput(self):
+        """Return the result of a jython command as a string."""
         while self.thread.running:
             time.sleep(0.1)
-        return self.thread.executionOutput
+        return self.thread.execution_output
 
 
 class CcsException(Exception):
+    """Exception class for CCS Jython interface."""
     def __init__(self, value):
+        super(CcsException, self).__init__()
         self.value = value
     def __str__(self):
         return repr(self.value)
 
 
 class CcsJythonInterpreter:
-    port = 4444
-    host = None
-    name = None
-
+    """Interface class to CCS Jython interpreter."""
     def __init__(self, name=None, host=None, port=4444):
-        CcsJythonInterpreter.port = port
+        self.port = port
         if host is None:
             # Get local machine name
-            CcsJythonInterpreter.host = socket.gethostname()
+            self.host = socket.gethostname()
         else:
-            CcsJythonInterpreter.host = host
-        host_and_port = '{}:{}'.format(CcsJythonInterpreter.host,
-                                       CcsJythonInterpreter.port)
+            self.host = host
+        host_and_port = '{}:{}'.format(self.host, self.port)
         try:
-            self.socketConnection = CcsJythonInterpreter.__establishSocketConnectionToCcsJythonInterpreter__()
-            print('Initialized connection to CCS Python interpreter on host',
+            self.socket_connection = self._socket_connection()
+            print('Connected to CCS Python interpreter on host:port',
                   host_and_port)
         except Exception as eobj:
             print(eobj)
-            raise CcsException("Could not establish a connection with " +
-                               "CCS Python Interpreter on host " +
-                               host_and_port)
-
+            raise CcsException("Could not connect to CCS Python Interpreter " +
+                               "on host:port " + host_and_port)
         if name is not None:
             name = name.replace("\n", "")
             self.syncExecution("initializeInterpreter " + name)
 
-    @staticmethod
-    def __establishSocketConnectionToCcsJythonInterpreter__():
-         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-         s.connect((CcsJythonInterpreter.host, CcsJythonInterpreter.port))
-         connectionResult = s.recv(1024).decode('utf-8')
-         if "ConnectionRefused" in connectionResult:
-            raise CcsException("Connection Refused ")
-         return s
+    def _socket_connection(self):
+        sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sc.connect((self.host, self.port))
+        connectionResult = sc.recv(1024).decode('utf-8')
+        if "ConnectionRefused" in connectionResult:
+            raise CcsException("Connection Refused")
+        return sc
 
     def aSyncExecution(self, statement):
         return self.sendInterpreterServer(statement)
 
     def syncExecution(self, statement):
         result = self.sendInterpreterServer(statement)
-        output = result.getOutput()
+        # Calling .getOutput() here causes the object to wait for the
+        # underlying thread to stop running.
+        result.getOutput()
         return result
 
-    def aSyncScriptExecution(self, fileName):
-        fo = open(fileName, "r")
-        fileContent = fo.read()
-        fo.close()
+    def aSyncScriptExecution(self, filename):
+        with open(filename, "r") as fd:
+            fileContent = fd.read()
         return self.sendInterpreterServer(fileContent)
 
-    def syncScriptExecution(self, fileName, setup_commands=(), verbose=False):
+    def syncScriptExecution(self, filename, setup_commands=(), verbose=False):
         if verbose and setup_commands:
-            print("Executing setup commands for", fileName)
+            print("Executing setup commands for", filename)
         for command in setup_commands:
             if verbose:
                 print(command)
             self.syncExecution(command)
 
         if verbose:
-            print("Executing %s..." % fileName)
-        with open(fileName, "r") as fd:
+            print("Executing %s..." % filename)
+        with open(filename, "r") as fd:
             fileContent = fd.read()
         result = self.sendInterpreterServer(fileContent)
-        output = result.getOutput()
+        # Calling .getOutput() here causes the object to wait for the
+        # underlying thread to stop running.
+        result.getOutput()
         return result
 
     def sendInterpreterServer(self, content):
-        threadId = (str(int(round(time.time() * 1000))) + "-" +
-                    str(random.randint(0,1000)))
-        thread = _CcsPythonExecutorThread(threadId, self.socketConnection)
-        thread.executePythonContent(content)
-        return CcsExecutionResult(thread)
+        thread_id = str(uuid.uuid4())
+        executor_thread = CcsPythonExecutorThread(thread_id,
+                                                  self.socket_connection)
+        return executor_thread.executePythonContent(content)
 
 
-class _CcsPythonExecutorThread:
-
-    def __init__(self, threadId, s):
-        self.s = s
-        self.threadId = threadId
-        self.outputThread = threading.Thread(target=self.listenToSocketOutput)
+class CcsPythonExecutorThread:
+    def __init__(self, thread_id, socket_connection):
+        self.socket_connection = socket_connection
+        self.thread_id = thread_id
+        self.output_thread = threading.Thread(target=self.listenToSocketOutput)
         self.java_exceptions = []
 
     def executePythonContent(self, content):
         self.running = True
-        self.outputThread.start()
-        content = ("startContent:" + self.threadId + "\n" +
-                   content + "\nendContent:" + self.threadId + "\n")
-        self.s.send(content.encode('utf-8'))
+        self.output_thread.start()
+        content = ("startContent:" + self.thread_id + "\n" +
+                   content + "\nendContent:" + self.thread_id + "\n")
+        self.socket_connection.send(content.encode('utf-8'))
         return CcsExecutionResult(self)
 
     def listenToSocketOutput(self):
         re_obj = re.compile(r'.*java.lang.\w*Exception.*')
-        self.executionOutput = ""
+        self.execution_output = ""
         while self.running:
             try:
-                output = self.s.recv(1024).decode('utf-8')
+                output = self.socket_connection.recv(1024).decode('utf-8')
             except Exception as eobj:
                 print(eobj)
                 raise CcsException("Communication Problem with Socket")
             for item in output.split('\n'):
                 if re_obj.match(item):
                     self.java_exceptions.append(item)
-            if "doneExecution:" + self.threadId not in output:
+            if "doneExecution:" + self.thread_id not in output:
                 sys.stdout.write(output)
                 sys.stdout.flush()
+                self.execution_output += output
             else:
                 self.running = False
-                self.executionOutput \
-                    = self.executionOutput.replace("doneExecution:" +
-                                                   self.threadId+"\n", "")
-        del self.outputThread
+        del self.output_thread
