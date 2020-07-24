4bc3705450ee698d6d3231c2d47a83c946fda089
James Chiang <jchiang@slac.stanford.edu>
2020-07-24 14:06:45 -0700

    enable rebinning; add an ad hoc fix for e2V sensors when using old camera yaml files

--- python/focal_plane_mosaics.py
+++ python/focal_plane_mosaics.py
@@ -6,8 +6,10 @@ layout in obs_lsst.
 import copy
 import numpy as np
 import lsst.afw.image as afwImage
+import lsst.afw.math as afwMath
 from lsst.afw.cameraGeom import utils as cgu
 import lsst.daf.persistence as dp
+
 
 __all__ = ['RawImageSource']
 
@@ -19,6 +21,9 @@ class RawImageSource:
     """
     ix = np.concatenate((np.arange(8), np.arange(7, -1, -1)))
     iy = np.concatenate((np.zeros(8, dtype=int), np.ones(8, dtype=int)))
+    e2V_rafts = ['R30', 'R31', 'R32', 'R33', 'R34',
+                        'R21', 'R22', 'R23', 'R24',
+                        'R11', 'R12', 'R13', 'R14']
     def __init__(self, butler, visit):
         """
         Parameters
@@ -33,7 +38,7 @@ class RawImageSource:
         self.isTrimmed = True
         self.background = 0
 
-    def getCcdImage(self, detector, imageFactory, binSize):
+    def getCcdImage(self, detector, imageFactory, binSize=1):
         """
         Mosaic the amplifier data from a raw file into a composed
         CCD with the prescan and overscan regions removed.
@@ -45,9 +50,8 @@ class RawImageSource:
         imageFactory: lsst.afw.Image
             Image class to use to make a Image object to pass to
             the cameraGeom mosaicking code.
-        binSize: int
-            Not used. This is needed for interface compatibility with
-            the cameraGeom code.
+        binSize: int [1]
+            Rebinning size in pixels.
 
         Returns
         -------
@@ -57,24 +61,33 @@ class RawImageSource:
         datarefs = self.butler.subset('raw_amp', visit=self.visit,
                                       detector=detector.getId())
         for dataref, amp_info in zip(datarefs, detector):
-            hdu = amp_info.get('hdu')
+            hdu = dataref.dataId['channel']
+            raft = dataref.dataId['raftName']
             raw_bbox = amp_info.getRawDataBBox()
             dx = raw_bbox.getWidth()
             dy = raw_bbox.getHeight()
             full_segment = dataref.get('raw_amp').getImage()
+            bias = full_segment[amp_info.getRawHorizontalOverscanBBox()]
+            full_segment -= afwMath.makeStatistics(bias,
+                                                   afwMath.MEANCLIP).getValue()
             imaging_segment = imageFactory(full_segment, raw_bbox)
             xoffset = self.ix[hdu-1]*dx
             yoffset = self.iy[hdu-1]*dy
             data = copy.deepcopy(imaging_segment.array)
-            if amp_info.getRawFlipX():
+            #if amp_info.getRawFlipX():  # This only works if the yaml files
+                                         # describing the camera are correct.
+            if (raft not in self.e2V_rafts) or hdu < 9:
                 data = data[:, ::-1]
             if amp_info.getRawFlipY():
                 data = data[::-1, :]
             image.array[yoffset: yoffset + dy, xoffset: xoffset + dx] += data
-        return (image, )
+        my_image = imageFactory(image.array[::-1, :])
+        if binSize > 1:
+            my_image = afwMath.binImage(my_image, binSize)
+        return (my_image, )
 
 
-def make_fp_mosaic(repo, visit, det_names=None, outfile=None):
+def make_fp_mosaic(repo, visit, det_names=None, outfile=None, bin_size=10):
     """
     Function to make a mosaic of raw image data in the LSST focal plane.
 
@@ -90,6 +103,8 @@ def make_fp_mosaic(repo, visit, det_names=None, outfile=None):
     outfile: str [None]
         Name of FITS file to write with the mosaicked data.  If None,
         then no file will be written.
+    bin_size: int [10]
+        Rebinning size in pixels.
 
     Returns
     -------
@@ -103,7 +118,7 @@ def make_fp_mosaic(repo, visit, det_names=None, outfile=None):
 
     image = cgu.makeImageFromCamera(camera, detectorNameList=det_names,
                                     imageSource=image_source,
-                                    imageFactory=afwImage.ImageF)
+                                    imageFactory=afwImage.ImageF, binSize=bin_size)
     if outfile is not None:
         image.writeFits(outfile)
 
